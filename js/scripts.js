971217faf87ffb0fc1b16b6dc5a682c805c26a7c
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-08-12 16:57:41 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function () {
+      $(".rslides").responsiveSlides({
+        speed: 800,
+		timeout: 8000,
+		pager: true,
+		nav: true
+      });
+});
+
+
+/*placeholder*/
+$(document).ready(function(){
+        $.Placeholder.init({ color : "#000000" });
+ });
+ 
+ /*menu*/
+$(".nav li").first().addClass('home');
+
+ 
